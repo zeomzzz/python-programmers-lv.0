5a8ba53a2eed7d2b7134663b9dbe501cc7c5c8e1
Jeongmin <107301060+zeomzzz@users.noreply.github.com>
2022-10-07 21:22:58 +0900

    Update 120804_두-수의-곱.py

--- 120804_두-수의-곱.py
+++ 120804_두-수의-곱.py
@@ -1,8 +1,3 @@
-#include <stdio.h>
-#include <stdbool.h>
-#include <stdlib.h>
-
-int solution(int num1, int num2) {
-    int answer = num1 * num2;
-    return answer;
-}
+def solution(num1, num2):
+    answer = num1 * num2
+    return answer
