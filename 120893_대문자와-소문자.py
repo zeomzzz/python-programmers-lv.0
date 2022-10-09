1e1f1b4d51c0c5a11e825ac88ba375fc12a7f350
Jeongmin <107301060+zeomzzz@users.noreply.github.com>
2022-10-09 18:37:03 +0900

    Update 120893_대문자와-소문자.py
    swapcase 풀이 추가

--- 120893_대문자와-소문자.py
+++ 120893_대문자와-소문자.py
@@ -1,3 +1,4 @@
+# 풀이1. 대소문자 판별 -> 변경
 def solution(my_string):
     answer = ''
     for i in my_string :
@@ -5,4 +6,9 @@ def solution(my_string):
             answer += i.lower()
         else :
             answer += i.upper()
+    return answer
+
+# 풀이2. swapcase 함수 사용
+def solution(my_string):
+    answer = my_string.swapcase()
     return answer
