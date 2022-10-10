26690747b4b94d74858c9e591f584be362113489
Jeongmin <107301060+zeomzzz@users.noreply.github.com>
2022-10-10 17:45:14 +0900

    Create 120841_점의-위치-구하기.py

---
+++ 120841_점의-위치-구하기.py
@@ -0,0 +1,12 @@
+def solution(dot):
+    if dot[0] > 0 :
+        if dot[1] > 0 :
+            answer = 1
+        else :
+            answer = 4
+    else :
+        if dot[1] > 0 :
+            answer = 2
+        else :
+            answer = 3
+    return answer
