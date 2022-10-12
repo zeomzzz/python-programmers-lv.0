e8572f83561ee34a065c599cb8d4d16e3b7b888b
Jeongmin <107301060+zeomzzz@users.noreply.github.com>
2022-10-12 22:48:30 +0900

    Update 120836_순서쌍의-개수.py

--- 120836_순서쌍의-개수.py
+++ 120836_순서쌍의-개수.py
@@ -1,3 +1,21 @@
+def solution(n):        
+    answer = 0
+    i = 1
+    
+    while True :
+        if i <= (n // i) :
+            if i * (n // i) == n :
+                if i != (n // i) :
+                    answer += 2
+                else :
+                    answer += 1
+            i += 1
+        else :
+            break
+    return answer
+
+'''
+# 이 코드는 일부 TC에서 코드 무한 로딩.. 왜..?
 def solution(n):
     answer = 0
     for i in range(0, n+1) :
@@ -5,3 +23,4 @@ def solution(n):
             if i*j == n :
                 answer += 1
     return answer
+'''
