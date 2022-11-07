9da620816be3a338ac93031f787705e424a3d134
Jeongmin <107301060+zeomzzz@users.noreply.github.com>
2022-11-07 22:44:19 +0900

    Update 120894_영어가 싫어요.py

--- 120894_영어가 싫어요.py
+++ 120894_영어가 싫어요.py
@@ -1,3 +1,25 @@
+# 두 번째 풀이 : replace 이용
+def solution(numbers):
+    answer = 0
+    
+    num_replaced = numbers.replace('zero', '0')
+    num_replaced = num_replaced.replace('one', '1')
+    num_replaced = num_replaced.replace('two', '2')
+    num_replaced = num_replaced.replace('three', '3')
+    num_replaced = num_replaced.replace('four', '4')
+    num_replaced = num_replaced.replace('five', '5')
+    num_replaced = num_replaced.replace('six', '6')
+    num_replaced = num_replaced.replace('seven', '7')
+    num_replaced = num_replaced.replace('eight', '8')
+    num_replaced = num_replaced.replace('nine', '9')
+    
+    answer = int(num_replaced)
+    
+    return answer
+
+
+# 첫 풀이 : slicing 이용하여 비교
+
 def solution(numbers):
     answer = 0
     num_list = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
