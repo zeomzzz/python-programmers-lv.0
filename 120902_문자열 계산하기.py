7ee7bd09c5a5528207580b09f8dacbfd1909f369
Jeongmin <107301060+zeomzzz@users.noreply.github.com>
2022-11-10 22:37:04 +0900

    Create 120902_문자열 계산하기.py

---
+++ 120902_문자열 계산하기.py
@@ -0,0 +1,15 @@
+def solution(my_string):
+    
+    # 띄어쓰기 기준으로 split
+    split_list = my_string.split(' ')
+    answer = int(split_list[0])
+    
+    
+    # 짝수 인덱스는 정수, 홀수 인덱스는 
+    for i in range(0, int(len(split_list) // 2)) :
+        if split_list[2 * i + 1] == '+' :
+            answer += int(split_list[2 * i + 2])
+        else :
+            answer -= int(split_list[2 * i + 2])
+    
+    return answer
