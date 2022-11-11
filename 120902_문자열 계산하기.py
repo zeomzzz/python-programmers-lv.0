084a5869406eabe34c771286ad1670c15b78d0b5
Jeongmin <107301060+zeomzzz@users.noreply.github.com>
2022-11-11 15:13:28 +0900

    Update

--- 120902_문자열 계산하기.py
+++ 120902_문자열 계산하기.py
@@ -4,8 +4,7 @@ def solution(my_string):
     split_list = my_string.split(' ')
     answer = int(split_list[0])
     
-    
-    # 짝수 인덱스는 정수, 홀수 인덱스는 
+    # 짝수 인덱스는 정수, 홀수 인덱스는 연산자임을 이용
     for i in range(0, int(len(split_list) // 2)) :
         if split_list[2 * i + 1] == '+' :
             answer += int(split_list[2 * i + 2])
