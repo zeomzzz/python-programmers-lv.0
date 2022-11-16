cd6f01a6492af6fb1aacbc57d82e6c9a315348dd
Jeongmin <107301060+zeomzzz@users.noreply.github.com>
2022-11-16 21:05:33 +0900

    Update 120871_저주의 숫자 3.py

--- 120871_저주의 숫자 3.py
+++ 120871_저주의 숫자 3.py
@@ -1,5 +1,7 @@
 # 3x 마을의 1 ~ 100 리스트 만들고 인덱스 이용하여 answer 반환
 
+# 리스트 만드는 방법 1 : 3의 배수 아님 & 10으로 나누어 나머지 3 아님 (각 자리수에 3이 없음)
+
 def solution(n):
     town_num = []
     i = 1
@@ -17,6 +19,23 @@ def solution(n):
         else :
             i += 1
         
+    answer = town_num[n - 1]
+    
+    return answer
+
+
+# 리스트 만드는 방법 2 : 3의 배수 아님 & str로 바꾸었을 떄 3 없음
+
+def solution(n):
+    answer = 0
+    town_num = []
+    i = 1
+    
+    while len(town_num) < 101 :
+        if i % 3 != 0 and '3' not in str(i) :
+            town_num.append(i)
+        i += 1
+    
     answer = town_num[n - 1]
     
     return answer
