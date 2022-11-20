bd8f30f66f95a48c011dc71250bcde3cf7ba5d18
Jeongmin <107301060+zeomzzz@users.noreply.github.com>
2022-11-21 00:05:39 +0900

    Update (미완료) 120866_안전지대.py

--- (미완료) 120866_안전지대.py
+++ (미완료) 120866_안전지대.py
@@ -1,3 +1,41 @@
+# 3차(통과) : list 복제했을 때 각 요소의 id도 달라지도록 deepcopy 
+def solution(board):
+    import copy
+    
+    answer = 0
+    padding = []
+    
+    while len(padding) < len(board) + 2 :
+        padding.append(2)
+    
+    for i in board :
+        i.insert(0, 2)
+        i.append(2)
+    
+    board.insert(0, padding)
+    board.append(padding)
+    
+    board_cp = copy.deepcopy(board)
+
+    for j in range(1, len(board)-1) :
+        for k in range(1, len(board)-1) :
+            if board[j][k] == 1 :
+                board_cp[j-1][k-1] = 2
+                board_cp[j-1][k] = 2
+                board_cp[j-1][k+1] = 2
+                board_cp[j][k-1] = 2
+                board_cp[j][k+1] = 2
+                board_cp[j+1][k-1] = 2
+                board_cp[j+1][k] = 2
+                board_cp[j+1][k+1] = 2
+    
+    for l in board_cp :
+        answer += l.count(0)
+
+    return answer
+
+
+
 # 1차 (정확성 63.0/100.0)
 def solution(board):
     answer = 0
